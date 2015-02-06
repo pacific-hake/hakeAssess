a3e32968ef122f0b9b881a5e790a348983a486e9
allanhicks <fishmanHicks@gmail.com>
2015-02-05 16:46:22 -0800

    added bridge models code

--- hake2015/Rcode/BridgeModels.R
+++ hake2015/Rcode/BridgeModels.R
@@ -11,6 +11,7 @@ hake2014.03 <- SS_output(dir=paste(SSdir,"0.3_2014hake_21_fixWtAtAge",sep="/"),c
 hake2014.04 <- SS_output(dir=paste(SSdir,"0.4_2014hake_21_updateComps",sep="/"),covar=T,verbose=F)
 hake2015.1 <- SS_output(dir=paste(SSdir,"01_2015hake_add2014data",sep="/"),covar=T,verbose=F,ncols=43)
 hake2015.2 <- SS_output(dir=paste(SSdir,"02_2015hake_redoFishComps",sep="/"),covar=T,verbose=F,ncols=43)
+hake2015.base <- SS_output(dir=paste(SSdir,"2015hake_basePreSRG",sep="/"),covar=T,verbose=F,ncols=43)
 
 
 #Bridge to new version and updating old data
@@ -20,6 +21,9 @@ mysummary <- SSsummarize(mymodels)
 pdf(paste(SSdir,"Figures/update2013data.pdf",sep="/"))
 SSplotComparisons(mysummary, legendlabels=modelnames,endyr=2015,new=F)
 dev.off()
+#For document
+SSplotComparisons(mysummary, legendlabels=modelnames,endyr=2015,png=T,plotdir="C:/NOAA2015/Hake/WriteUp/Figures/Bridge")
+
 
 #Bridge to add 2014
 mymodels <- list(hake2014.04,hake2015.1)
@@ -41,3 +45,11 @@ SSplotComparisons(mysummary, legendlabels=modelnames,endyr=2015,new=F,
 				  densitynames=c("NatM_p_1_Fem_GP_1","Main_RecrDev_2010","Late_RecrDev_2011","SPB_Virgin","Bratio_2015","ForeCatch_2015","Recr_2010","Recr_2011"))
 dev.off()
 
+
+
+#Bridge to 2015 base
+mymodels <- list(hake2014.04,hake2015.base)
+modelnames <- c("Update 2013 data","Add 2014 data")
+mysummary <- SSsummarize(mymodels)
+#For document
+SSplotComparisons(mysummary, legendlabels=modelnames,endyr=2015,png=T,plotdir="C:/NOAA2015/Hake/WriteUp/Figures/BridgeToBase")
